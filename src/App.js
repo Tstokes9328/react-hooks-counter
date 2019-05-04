e0c4682d6657ad15c63ddab5ba6ce3c15c3c7b95
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-04 13:37:32 -0600

    refactored the class component to a functional component using useState hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState } from 'react';
 
-function App() {
+//Import the useState hook in from react
+  //useState is a new react hook that will allow us to have a state within a functional component
+
+
+const App = () => {
+  //this is a new react hook to create state
+    //the first value is a value to keep track of count
+    //the second is a function that will update state
+    //we pass in 0 to useState to define that count will be defaulted to 0
+  const [count, setCount] = useState(0);
+
+  //this is the method to increment count on state
+  const increment = () => {
+    //call the function setCount to update count on state
+    setCount(count + 1);
+  };
+
+  //return jsx to display
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Counter App</h2>
+      <button onClick={increment}>Clicked: {count}</button>
     </div>
-  );
-}
+  )
+
+};
 
 export default App;
